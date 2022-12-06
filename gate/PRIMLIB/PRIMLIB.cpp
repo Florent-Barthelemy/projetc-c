6c8beffd398b8c73d9e94c3610c2dca6760463df
Nathan Hocquette <nathan.hocquette@free.fr>
2022-12-06 13:54:47 +0100

    Implemented default 1-2 input gates of PRIMLIB

--- gate/PRIMLIB/PRIMLIB.cpp
+++ gate/PRIMLIB/PRIMLIB.cpp
@@ -1,11 +1,36 @@
 #include "PRIMLIB.h"
 
+void INV::updateState()
+{
+    getOutConn("OUT1")->state = !getInConn("IN1")->state;
+}
+
 void AND::updateState()
 {
     getOutConn("OUT1")->state = getInConn("IN1")->state * getInConn("IN2")->state;
 }
 
+void NAND::updateState()
+{
+    getOutConn("OUT1")->state = !(getInConn("IN1")->state * getInConn("IN2")->state);
+}
+
 void OR::updateState()
 {
     getOutConn("OUT1")->state = getInConn("IN1")->state + getInConn("IN2")->state;
+}
+
+void NOR::updateState()
+{
+    getOutConn("OUT1")->state = !(getInConn("IN1")->state + getInConn("IN2")->state);
+}
+
+void XOR::updateState()
+{
+    getOutConn("OUT1")->state = getInConn("IN1")->state ^ getInConn("IN2")->state;
+}
+
+void NXOR::updateState()
+{
+    getOutConn("OUT1")->state = !(getInConn("IN1")->state ^ getInConn("IN2")->state);
 }
