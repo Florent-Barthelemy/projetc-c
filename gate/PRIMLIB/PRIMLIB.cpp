4c53d6191bde324d8b6defd4591df693cee32c74
Florent <florent.barthelemy@protonmail.com>
2022-12-08 10:16:11 +0100

    Up to date primlib

--- gate/PRIMLIB/PRIMLIB.cpp
+++ gate/PRIMLIB/PRIMLIB.cpp
@@ -1,9 +1,18 @@
 #include "PRIMLIB.h"
 
+//combinatorial 1i/1s gates
+
+void BUFF::updateState()
+{
+    getOutConn("OUT1")->state = getInConn("IN1")->state;
+}
+
 void INV::updateState()
 {
     getOutConn("OUT1")->state = !getInConn("IN1")->state;
 }
+
+//combinatorial 2i/1s gates
 
 void AND::updateState()
 {
@@ -35,42 +44,23 @@ void NXOR::updateState()
     getOutConn("OUT1")->state = !(getInConn("IN1")->state ^ getInConn("IN2")->state);
 }
 
+//1 bit memories
 
-
-SIM_NODE* invCreator(INIT_PARAM params)
+void DFF::updateState()
 {
-    return new INV(params.nodeName);
+    if(getInConn("Clk")->state == H && this->prevClkState == L)
+        this->currState = getInConn("D")->state;
+
+    this->prevClkState = getInConn("Clk")->state;
+
+    getOutConn("Q")->state = this->currState;
 }
 
-SIM_NODE* andCreator(INIT_PARAM params)
+void DLATCH::updateState()
 {
-    return new AND(params.nodeName);
+    if(getInConn("Clk")->state == H )
+        this->currState = getInConn("D")->state;
+        
+    getOutConn("Q")->state = this->currState;
 }
-
-SIM_NODE* nandCreator(INIT_PARAM params)
-{
-    return new NAND(params.nodeName);
-}
-
-SIM_NODE* orCreator(INIT_PARAM params)
-{
-    return new OR(params.nodeName);
-}
-
-SIM_NODE* norCreator(INIT_PARAM params)
-{
-    return new NOR(params.nodeName);
-}
-
-SIM_NODE* xorCreator(INIT_PARAM params)
-{
-    return new XOR(params.nodeName);
-}
-
-SIM_NODE* nxorCreator(INIT_PARAM params)
-{
-    return new NXOR(params.nodeName);
-}
-
-
 
