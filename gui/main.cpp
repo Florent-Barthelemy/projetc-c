336643e3d06ff632e7847b9825a0e2b5ca6bf31a
Nathan Hocquette <nathan.hocquette@free.fr>
2022-12-15 13:05:21 +0100

    Main update

--- gui/main.cpp
+++ gui/main.cpp
@@ -5,7 +5,8 @@
 /*int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    MainWindow w(*new ACQUISITION_HANDLER(), *new STIMULI_HANDLER());
     w.show();
     return a.exec();
-}*/
+}
+*/
