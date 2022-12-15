b0f9d6e0c2cb1ee404587d0bb3f38665e0620c26
Nathan Hocquette <nathan.hocquette@free.fr>
2022-12-15 09:29:45 +0100

    help message file

---
+++ TOP/help.h
@@ -0,0 +1,14 @@
+#ifndef HELP_H_
+#define HELP_H_
+
+#define HELP_MSG    "wind [args]\n" \
+                    "\t--timestamp, -t: sets the simulation time. Default is 20." \
+                    "\t--help, -h: displays this help message." \
+                    "\t--outputFile, -o: output wavedrom file name." \
+                    "\t--setDeltaCycles, -dlt: sets the maximum delta cycle. Default is 10." \
+                    "\t--compilHistoryName, -chn: stimuli file name." \
+                    "\t--sti, -s: stimuli file name." \
+                    "\t--HDLfile, -f: circuit(s) to simulate." \
+                    "\t--dotCompatible, -dc: activate compatibility mode for dot files."
+
+#endif
